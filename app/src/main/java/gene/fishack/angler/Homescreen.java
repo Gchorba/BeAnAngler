ad5b387207944f6be0e14d0a4798964b6c203b30
Gene Chorba <Gchorba@gmail.com>
2015-06-17 21:38:58 -0400

    Homescreen with slide out menu added

--- app/src/main/java/gene/fishack/angler/Homescreen.java
+++ app/src/main/java/gene/fishack/angler/Homescreen.java
@@ -5,22 +5,99 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Homescreen extends Activity{
-	
+	private String[] drawerListViewItems;
+	private DrawerLayout drawerLayout;
+	private ListView drawerListView;
+	private ActionBarDrawerToggle actionBarDrawerToggle;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_homescreen);
+		// get list items from strings.xml
+		drawerListViewItems = getResources().getStringArray(R.array.items);
+		// get ListView defined in activity_main.xml
+		drawerListView = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		drawerListView.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.listview_item_row, drawerListViewItems));
+
+		// 2. App Icon
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+		// 2.1 create ActionBarDrawerToggle
+		actionBarDrawerToggle = new ActionBarDrawerToggle(
+				this,                  /* host Activity */
+				drawerLayout,         /* DrawerLayout object */
+				R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+				R.string.drawer_open,  /* "open drawer" description */
+				R.string.drawer_close  /* "close drawer" description */
+		);
+
+		// 2.2 Set actionBarDrawerToggle as the DrawerListener
+		drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+		// 2.3 enable and show "up" arrow
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		// just styling option
+		//drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+		drawerListView.setOnItemClickListener(new DrawerItemClickListener());
 	}
-	
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		actionBarDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		actionBarDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		// call ActionBarDrawerToggle.onOptionsItemSelected(), if it returns true
+		// then it has handled the app icon touch event
+
+		if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView parent, View view, int position, long id) {
+			Toast.makeText(Homescreen.this, ((TextView) view).getText(), Toast.LENGTH_LONG).show();
+			drawerLayout.closeDrawer(drawerListView);
+
+		}
+	}
+
 	public void onClickLakesButton(View v) {
 			Intent launchFragment = new Intent();
 			launchFragment.setClass(getApplicationContext(), ItemListActivity.class);
